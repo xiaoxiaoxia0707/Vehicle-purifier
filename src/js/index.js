855210c6b8e4116842d1b757c36b814e6d26d9fb
sasha <1217435098@qq.com>
2017-03-06 15:09:42 +0800

    实验

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+$(function() {
+    var deviceWidth = document.documentElement.clientWidth;
+    if (deviceWidth > 640) deviceWidth = 640;
+    document.documentElement.style.fontSize = deviceWidth / 6.4 + 'px';
+});
